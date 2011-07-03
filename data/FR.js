132cfdfd91ff0af39b506a7359617a13dc59b0e6
Thibault DURAND <tibo.durand@gmail.com>
2011-07-03 17:43:28 +0200

    Externalize data, templating with ICanHaz.js and Mustache

---
+++ data/FR.js
@@ -0,0 +1,141 @@
+var data_FR = {
+    title : "Thibault Durand",
+    subtitle : "Un passionné des technologies web",
+    nav : {
+        education : "Formation",
+        skills : "Compétences",
+        experiences : "Expériences",
+        achievements : "Réalisations",
+        interests : "Centres d'intérêts",
+        contact : "Contact"
+    },
+    education : {
+        label : "Formation",
+        icon : "notepad",
+        list : [
+            {
+                period : "2011/2012",
+                content : "5<sup>eme</sup> année à l'UFC de Fortaleza au Brésil, département Télé-Informatique"
+            },
+            {
+                period : "2009/2011",
+                content : "3<sup>eme</sup> et 4<sup>eme</sup> année au département Informatique de l'INSA de Lyon<br /><i>Génie logiciel, Système d’information, Interface utilisateur, Gestion de projet, Démarche qualité...</i>"
+            },
+            {
+                period : "2007/2009",
+                content : "Classe préparatoires intégrées à l'INSA de Lyon"
+            },
+            {
+                period : "2007",
+                content : "Bac S scientifique mention Bien -spécialité Mathématiques à Gueret (23)"
+            }
+        ]
+    },
+    skills : {
+        label : "Compétences",
+        icon : "star",
+        articles : [
+            {
+                title : "Langue",
+                list : [
+                    {
+                        content : "Anglais courant"
+                    },
+                    {
+                        content : "Espagnol lu / écrit / parlé"
+                    },
+                    {
+                        content : "Russe : notions"
+                    },
+                    {
+                        content : "Portugais : actuellement au Brésil ;-)"
+                    }
+                    ]
+            },
+            {
+                title : "Informatique",
+                list : [
+                    {
+                        content : "Outils : Eclipse, The Gimp, Git / SVN, Matlab"
+                    },
+                    {
+                        content : "Web : HTML5, CSS3, JavaScript, J2EE , XML/JSON"
+                    },
+                    {
+                        content : "Frameworks : jQuery/KnockoutJS/Backbone.js , Play! Framework"
+                    },
+                    {
+                        content : "Modélisation : UML, Merise"
+                    },
+                    {
+                        content : "Languages : Java, Scala, C/C++, PHP, SQL"
+                    },
+                    {
+                        content : "Autre: Nuxeo (GED), Apache/Tomcat, Ant"
+                    },
+                    {
+                        content : "Systèmes : Linux/Unix, Mac OSX, Windows Server, VxWorks"
+                    }
+                    ]
+            }
+        ]
+    },
+    experiences : {
+        label : "Expériences",
+        icon : "statistic",
+        list : [
+            {
+                date : "2010",
+                duration : "2 mois",
+                content : "mission de Développement : GED Open Source à ONYX Informatique (69) auto-formation sur le Framework Nuxeo, réalisation d’un audit client, identification du besoin, conception et réalisation de plugins spécifiques."
+            },
+            {
+                date : "2009",
+                duration : "1,5 mois",
+                content : "mission de Responsable Informatique à Cartonnerie JEAN (23) administration réseau, gestion	d’un système d’information,	maintenance matérielle, Windows Server 2003, Wavesoft."
+            },
+            {
+                date : "2008",
+                duration : "1 mois",
+                content : "stage ouvrier à SAUTHON Industrie– Service Emballage – Guéret (23) travail en équipe, à la chaîne."
+            },
+            {
+                date : "2007",
+                duration : "1,5 mois",
+                content : "mission de Développement : application de gestion de stock à Cartonnerie JEAN identification du besoin, conception de l’application, gestion des données, développement, application Web, PHP, SQL."
+            },
+            {
+                date : "2006",
+                duration : "1,5 mois",
+                content : "mission de Développement d’un site internet pour Cartonnerie JEAN collecte d’information, synthèse, réalisation graphique, traitement d’images."
+            },
+            {
+                date : "2009-2011",
+                duration : "3 ans",
+                content : "Président de l’association de canoë-kayak de l’INSA de Lyon organisation de Week-end rafting/canoë (pour 40 participants), encadrement de sorties d’initiation, prospection sponsors, logistique, trésorerie, communication"
+            }
+            
+        ]
+    },
+    achievements : {
+        label : "Réalisations",
+        icon : "tag"
+    },
+    interests : {
+        label : "Centres d'intérêts",
+        icon : "favourites",
+        content : "Voyages, Sports : Raids multisports (VTT, Trail, Course d’orientation), Canoë-Kayak, Rando ..."
+    },
+    contact : {
+        label : "Contact",
+        icon : "user-man",
+        list : [
+            {
+                content : "Email : tibo.durand+cv@gmail.com",
+            },
+            {
+                content : "Skype : tib-web",
+            },
+        ]
+    }
+};
